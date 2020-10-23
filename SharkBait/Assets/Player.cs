3a9e8fe24a1c674631b5f9d86b179605e35f10cc
Borkowski, Mark Daniel <markborkowski69@yahoo.com>
2020-10-23 00:02:21 -0400

    LoopingBackground
    Background GameObjects loop as the camera moves in the x and y directions.

---
+++ SharkBait/Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+       public Vector2 speed = new Vector2(10,10);
+       private vector2 movement;
+       private float inputX;
+       private float inputY;
+
+       void Start(){
+           
+       } 
+
+}
