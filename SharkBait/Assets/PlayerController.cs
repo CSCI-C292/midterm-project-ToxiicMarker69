90d2116c2aa8163aa3108f29888996ca110e3da7
Borkowski, Mark Daniel <markborkowski69@yahoo.com>
2020-10-23 21:30:31 -0400

    Player+Whale
    Movement

---
+++ SharkBait/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float movementSpeed = 6f;   
+    void Update()
+        {
+            //get the Input from Horizontal axis
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            //get the Input from Vertical axis
+            float verticalInput = Input.GetAxisRaw("Vertical");
+
+            //update the position
+            transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+
+            //output to log the position change
+            Debug.Log(transform.position);
+        }
+}
---
+++ SharkBait/Assets/WhaleSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleSpawner : MonoBehaviour
+{
+    float _yMin;
+    float _yMax;
+    float _xSpawn;
+    float _randI;
+    [SerializeField] GameObject _enemyPrefab;
+    
+    void Start()
+    {
+        
+        _yMin = Camera.main.ViewportToWorldPoint(new Vector3(0,.2f,0)).y;
+        _yMax = Camera.main.ViewportToWorldPoint(new Vector3(0,.4f,0)).y;
+        _xSpawn = Camera.main.ViewportToWorldPoint(new Vector3(1.5f, 0,0)).x;
+
+
+        InvokeRepeating("SpawnEnemy", 0, 15f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnEnemy() {
+        float randY = Random.Range( _yMin, _yMax);
+        Instantiate(_enemyPrefab, new Vector3(_xSpawn,randY, 0) , Quaternion.identity, this.transform);
+    }
+}
